0f04acf04c1432d03b89feb903b16607047297fc
Maxim Zavidei <maxim-zavidei+git@pm.me>
2023-01-27 23:13:42 +0100

    Added logging

--- src/Jobs.Identity/Program.cs
+++ src/Jobs.Identity/Program.cs
@@ -1,4 +1,16 @@
 using Jobs.Identity.Config;
+using Serilog;
+using Serilog.Events;
+
+Log.Logger = new LoggerConfiguration()
+.MinimumLevel.Debug()
+.MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+.MinimumLevel.Override("Microsoft.Hosting.Lifetime", LogEventLevel.Information)
+.MinimumLevel.Override("System", LogEventLevel.Warning)
+.MinimumLevel.Override("Microsoft.AspNetCore.Authentication", LogEventLevel.Information)
+.Enrich.FromLogContext()
+.WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss}] {Level} {SourceContext}")
+.CreateLogger();
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +20,11 @@ builder.Services.AddIdentityServer()
     .AddInMemoryIdentityResources(InMemoryConfig.IdentityResources)
     .AddInMemoryClients(InMemoryConfig.Clients)
     .AddTestUsers(InMemoryConfig.TestUsers)
+    .AddInMemoryApiResources(InMemoryConfig.ApiResources)
+    .AddInMemoryApiScopes(InMemoryConfig.ApiScopes)
     .AddDeveloperSigningCredential();
+
+builder.Host.UseSerilog();
 
 var app = builder.Build();
 
